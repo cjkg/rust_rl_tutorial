521ffbce07f86caf9b3fb39e2183f0c5aabef6e2
Chris Gibilisco <cjgibilisco@gmail.com>
2020-02-18 22:31:23 -0600

    Added Dungeon Generator

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,18 @@
 
 use tcod::colors::*;
 use tcod::console::*;
+use std::cmp; 
+use rand::Rng;
+
 //TODO: Constants file
 // actual size of the window:
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
+
+//TODO: Dungeon Generator file
+const ROOM_MAX_SIZE: i32 = 10;
+const ROOM_MIN_SIZE: i32 = 6;
+const MAX_ROOMS: i32 = 30;
 
 //size of the game map:
 const MAP_WIDTH: i32 = 80;
@@ -70,6 +78,19 @@ impl Rect {
             y2: y + h,
         }
     }
+
+    pub fn center(&self) -> (i32, i32) {
+        let center_x = (self.x1 + self.x2) / 2;
+        let center_y = (self.y1 + self.y2) / 2;
+        (center_x, center_y)
+    }
+
+    pub fn intersects_with(&self, other: &Rect) -> bool {
+        (self.x1 <= other.x2)
+            && (self.x2 >= other.x1)
+            && (self.y1 <= other.y2)
+            && (self.y2 >= other.y1)
+    }
 }
 
 //TODO: object source file
@@ -110,17 +131,73 @@ fn create_room(room: Rect, map: &mut Map) {
     }
 }
 
+//TODO: dungeon generator source file
+
+fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
+    //horizontal tunnel digger
+    for x in cmp::min(x1, x2)..(cmp::max(x1, x2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+//TODO: dungeon generator source file
+fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
+    //vert tunnel digger
+    for y in cmp::min(y1, y2)..(cmp::max(y1, y2) + 1) {
+        map[x as usize][y as usize] = Tile::empty()
+    }
+}
 
 //TODO: Map source file
-fn make_map() -> Map {
+fn make_map(player: &mut Object) -> Map {
     //fill map with "blocked" tiles
     let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
-    //TODO: DEBUG
     
-    let room1 = Rect::new(20, 15, 10, 15);
-    let room2 = Rect::new(50, 15, 10, 15);
-    create_room(room1, &mut map);
-    create_room(room2, &mut map);
+    let mut rooms = vec![];
+
+    for _ in 0..MAX_ROOMS {
+        // random width and height
+        let w = rand::thread_rng().gen_range(ROOM_MIN_SIZE, ROOM_MAX_SIZE + 1);
+        let h = rand::thread_rng().gen_range(ROOM_MIN_SIZE, ROOM_MAX_SIZE + 1);
+        // random position without going out of the boundaries of the map
+        let x = rand::thread_rng().gen_range(0, MAP_WIDTH - w);
+        let y = rand::thread_rng().gen_range(0, MAP_HEIGHT - h);
+
+        let new_room = Rect::new(x, y, w, h);
+
+        let failed = rooms
+            .iter()
+            .any(|other_room| new_room.intersects_with(other_room));
+
+        if !failed {
+            create_room(new_room, &mut map);
+
+            let (new_x, new_y) = new_room.center();
+
+            if rooms.is_empty() {
+                //this is the first room
+                player.x = new_x;
+                player.y = new_y;
+            } else {
+                //otherwise, let's dig a tunnel
+                
+                //previous room's center:
+                let (prev_x, prev_y) = rooms[rooms.len() - 1].center();
+
+                if rand::random() {
+                    create_h_tunnel(prev_x, new_x, prev_y, &mut map);
+                    create_v_tunnel(prev_y, new_y, new_x, &mut map);
+                } else {
+                    create_v_tunnel(prev_y, new_y, prev_x, &mut map);
+                    create_h_tunnel(prev_x, new_x, new_y, &mut map);
+                }
+            }
+            //finally, append the new room to the list
+            rooms.push(new_room);
+        }
+        
+
+    }
 
     map
 }
@@ -211,7 +288,7 @@ fn main() {
     let mut objects = [player, npc];
 
     let game = Game {
-        map: make_map(),
+        map: make_map(&mut objects[0]),
     };
     while !tcod.root.window_closed() {
         // clear the screen of the previous frame
